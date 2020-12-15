9b82dd5f5688316c3ae9233199682312578d9d9a
eryzna <eryzna@gmail.com>
2020-12-15 17:49:25 -0500

    link to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'static#home'
+  root 'static#static'
+
+  get '/welcome', to: 'application#index'
+
+  get '/swaps', to: 'swaps#index', as: 'swaps'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
