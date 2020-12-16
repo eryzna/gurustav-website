43db773db5a627ac52312f4767fa4a17ccc9ede5
eryzna <eryzna@gmail.com>
2020-12-16 16:25:46 -0500

    pages and styling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/welcome', to: 'application#index'
 
-  get '/swaps', to: 'swaps#index', as: 'swaps'
+  get '/about', to: 'application#about'
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
